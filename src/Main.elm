95bde8fecfb434cb0efa69241cb59765143c2739
Fredrik Fossum <fredrik.fossum@finn.no>
2019-06-25 19:20:12 +0200

    format

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,29 @@
-port module Main exposing (..)
+port module Main exposing (Model, Msg(..), init, main, receiveMessage, sendMessage, subscriptions, update, view, viewLink)
 
 import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import Url
-import Json.Encode as E
 import Json.Decode as D
+import Json.Encode as E
+import Url
+
+
 
 -- MAIN
 
 
 main : Program () Model Msg
 main =
-  Browser.application
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    , onUrlChange = UrlChanged
-    , onUrlRequest = LinkClicked
-    }
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
 
 
 
@@ -29,15 +31,15 @@ main =
 
 
 type alias Model =
-  { key : Nav.Key
-  , url : Url.Url
-  , receivedMessages : List String
-  }
+    { key : Nav.Key
+    , url : Url.Url
+    , receivedMessages : List String
+    }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  ( Model key url [], Cmd.none )
+    ( Model key url [], Cmd.none )
 
 
 
@@ -45,33 +47,40 @@ init flags url key =
 
 
 type Msg
-  = LinkClicked Browser.UrlRequest
-  | UrlChanged Url.Url
-  | MessageReceived String
-  | SendMessageClicked
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | MessageReceived String
+    | SendMessageClicked
+
 
 port sendMessage : E.Value -> Cmd msg
 
+
 port receiveMessage : (D.Value -> msg) -> Sub msg
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    LinkClicked urlRequest ->
-      case urlRequest of
-        Browser.Internal url ->
-          ( model, Nav.pushUrl model.key (Url.toString url) )
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
 
-        Browser.External href ->
-          ( model, Nav.load href )
+                Browser.External href ->
+                    ( model, Nav.load href )
 
-    UrlChanged url ->
-      ( { model | url = url }
-      , Cmd.none
-      )
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
 
-    SendMessageClicked -> ( model, sendMessage (E.string "Hello") )
-    MessageReceived m -> ( { model | receivedMessages = m :: model.receivedMessages }, Cmd.none )
+        SendMessageClicked ->
+            ( model, sendMessage (E.string "Hello") )
+
+        MessageReceived m ->
+            ( { model | receivedMessages = m :: model.receivedMessages }, Cmd.none )
+
 
 
 -- SUBSCRIPTIONS
@@ -79,11 +88,15 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  receiveMessage (\v -> case D.decodeValue D.string v of
-    (Ok str) -> MessageReceived str
-    _ -> MessageReceived "Error parsing message"
-  )
+    receiveMessage
+        (\v ->
+            case D.decodeValue D.string v of
+                Ok str ->
+                    MessageReceived str
 
+                _ ->
+                    MessageReceived "Error parsing message"
+        )
 
 
 
@@ -92,22 +105,23 @@ subscriptions _ =
 
 view : Model -> Browser.Document Msg
 view model =
-  { title = "URL Interceptor"
-  , body =
-      [ text "The current URL is: "
-      , b [] [ text (Url.toString model.url) ]
-      , ul []
-          [ viewLink "/home"
-          , viewLink "/profile"
-          , viewLink "/reviews/the-century-of-the-self"
-          , viewLink "/reviews/public-opinion"
-          , viewLink "/reviews/shah-of-shahs"
-          ]
-      , button [ onClick SendMessageClicked ] [ text "Send message!" ]
-      , ul [] (List.map (\msg -> li [] [ text msg ]) model.receivedMessages)
-      ]
-  }
+    { title = "URL Interceptor"
+    , body =
+        [ text "The current URL is: "
+        , b [] [ text (Url.toString model.url) ]
+        , ul []
+            [ viewLink "/home"
+            , viewLink "/profile"
+            , viewLink "/reviews/the-century-of-the-self"
+            , viewLink "/reviews/public-opinion"
+            , viewLink "/reviews/shah-of-shahs"
+            ]
+        , button [ onClick SendMessageClicked ] [ text "Send message!" ]
+        , ul [] (List.map (\msg -> li [] [ text msg ]) model.receivedMessages)
+        ]
+    }
+
 
 viewLink : String -> Html msg
 viewLink path =
-  li [] [ a [ href path ] [ text path ] ]
+    li [] [ a [ href path ] [ text path ] ]
